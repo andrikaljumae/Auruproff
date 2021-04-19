3c983510eb2ebadc2f118991a0c50dfdaeff24ab
Andri Kaljumäe <43129486+andrikaljumae@users.noreply.github.com>
2021-04-19 15:08:20 +0300

    title fixed, navbar css needs update

--- src/Title.js
+++ src/Title.js
@@ -5,18 +5,20 @@ import Navbar from './components/Navbar';
 
 function Title() {
   return (
-    <div className="Title">
-        <div className="Header-left">
-            <img src={logo} width="150px" height="30px" alt="pilt" />
-        </div>
-        <Navbar/>
-        <div className="Header-middle">
-            <p>AURUPESU | SURVEPESU | KAITSETÖÖTLUS | JÄÄPUHASTUS |</p>
-            <p style={{fontSize: 22}}>Auruproff on väikeettevõte, mille taga on pühendunud meeskond.</p>
-            <p style={{fontSize: 22}}>Iga objekt on unikaalne ning teeme just Sinu soovidest ja vajadustest lähtuva pakkumise!</p>
-            <br></br>
-            <b>SOOVIN HINNAPAKKUMIST!</b>
-        </div>
+    <div className="wrapper">
+      <div className="Title">
+          <div className="Header-right">
+              <img src={logo} width="150px" height="30px" alt="pilt" />
+          </div>
+          <Navbar/>
+          <div className="Header-middle">
+              <p style={{fontSize: 34}}>AURUPESU | SURVEPESU | KAITSETÖÖTLUS | JÄÄPUHASTUS |</p>
+              <p style={{fontSize: 22}}>Auruproff on väikeettevõte, mille taga on pühendunud meeskond.</p>
+              <p style={{fontSize: 22}}>Iga objekt on unikaalne ning teeme just Sinu soovidest ja vajadustest lähtuva pakkumise!</p>
+              <br></br>
+              <b style={{fontSize: 34}}>SOOVIN HINNAPAKKUMIST!</b>
+          </div>
+      </div>
     </div>
   )
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import * as FaIcons from 'react-icons/fa';
-import * as AiIcons from 'react-icons/ai';
 import { Link } from 'react-router-dom';
 import { SidebarData } from './SidebarData';
 import './Navbar.css';
@@ -21,11 +20,6 @@ function Navbar() {
         </div>
         <nav className={sidebar ? 'nav-menu active' : 'nav-menu'}>
           <ul className='nav-menu-items' onClick={showSidebar}>
-            <li className='navbar-toggle'>
-              <Link to='#' className='menu-bars'>
-                <AiIcons.AiOutlineClose />
-              </Link>
-            </li>
             {SidebarData.map((item, index) => {
               return (
                 <li key={index} className={item.cName}>
